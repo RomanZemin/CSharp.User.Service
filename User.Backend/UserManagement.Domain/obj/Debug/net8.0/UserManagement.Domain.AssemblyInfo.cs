c6adffa4a4b4448ed76fbf9a1759261705d50afc
RomanZemin <RomanZemin12@yandex.ru>
2024-09-02 17:12:21 +0700

    fix

--- User.Backend/UserManagement.Domain/obj/Debug/net8.0/UserManagement.Domain.AssemblyInfo.cs
+++ User.Backend/UserManagement.Domain/obj/Debug/net8.0/UserManagement.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f447f2ec0392cbfc89ee3c1f67ea5becc858672a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e0bea4fad7ec236bfca31f6985fdb83cd376f76")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
